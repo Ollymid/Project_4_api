b5f6557a05aebb6cf64eadfc96d612562c5ce529
Ollymid <ollymiddleton@hotmail.co.uk>
2017-07-28 00:57:39 +0100

    backend working apart from delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   scope :api do
     resources :logs
     resources :dive_sites
-    resources :users, except: [:create]
+    resources :users, except: [:create, :update]
     post 'oauth/facebook'
     post 'register', to: 'authentications#register'
     post 'login', to: 'authentications#login'
